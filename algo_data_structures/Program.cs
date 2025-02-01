e54ac7c306a31b72f22b1e317b5c78314c2a8a94
Jakub Grobelny <72096815+kinsin5@users.noreply.github.com>
2025-02-01 18:58:26 +0100

    sort_project

---
+++ algo_data_structures/Program.cs
@@ -0,0 +1,266 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Projekt3
+{
+    internal class Program
+    {
+        static sorts sort  = new sorts();
+        static int[] t = new int[50000];
+        const int NIter = 3; // FOR QUICK SORT
+        //const int NIter = 1; // FOR BASIC SORT
+        const int Consta = 5;
+        static Random rnd = new Random(Guid.NewGuid().GetHashCode());
+        static void GenerateAscendingArray(int[] t)
+        {
+            for (int i = 0; i < t.Length; ++i) t[i] = i;
+            //return t;
+        }
+        static void GenerateDescendingArray(int[] t)
+        {
+            for (int i = 0; i < t.Length; ++i) t[i] = t.Length - i - 1;
+            //return t;
+        }
+        static void GenerateVShape(int[] t)
+        {
+            int n = t.Length;
+            int middle = n / 2;
+            for (int i = 0; i < middle; i++)
+            {
+                t[i] = n - 1 - (i * 2);
+            }
+            for (int i = middle; i < n; i ++)
+            {
+                t[i] = (i - middle) * 2;
+            }
+            //return t;
+        }
+        static void GenerateAShape(int[] t)
+        {
+            int n = t.Length;
+            int middle = n / 2;
+            for (int i = 0; i < middle; i++)
+            {
+                t[i] = i * 2;
+            }
+            for (int i = middle; i < n; i++)
+            {
+                t[i] = n - 1 - (i - middle) * 2;
+            }
+            //return t;
+        }
+        static void GenerateRandomArray(int[] t, Random rnd, int maxValue = int.MaxValue)
+        {
+            for (int i = 0; i < t.Length; ++i)
+                t[i] = rnd.Next(maxValue);
+        }
+
+        static void GenerateConst(int[] t)
+        {
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i] = Consta;
+            }
+        }
+        static string SortTimeQuick(int c, int[] t) //TIME FOR QUICKSORT WITH DIFFRENT PIVOT VALUES
+        {
+            double ElapsedSeconds;
+            long ElapsedTime = 0, MinTime = long.MaxValue, MaxTime = long.MinValue, IterationElapsedTime;
+
+            for (int i = 0; i < (NIter + 2); ++i)
+            {
+
+                long start = Stopwatch.GetTimestamp();
+                sort.qsort_it(t, c);
+                long end = Stopwatch.GetTimestamp();
+                IterationElapsedTime = end - start;
+                ElapsedTime += IterationElapsedTime;
+                if (IterationElapsedTime > MaxTime) MaxTime = IterationElapsedTime;
+                if (IterationElapsedTime < MinTime) MinTime = IterationElapsedTime;
+            }
+            ElapsedTime -= MaxTime + MinTime;
+            ElapsedSeconds = ElapsedTime * (1.0 / (NIter * Stopwatch.Frequency));
+            Console.Write("\t" + ElapsedSeconds.ToString("F10"));
+            return ElapsedSeconds.ToString("F10");
+        }
+        static string SortTime(int c,int[] t)
+        {
+            double ElapsedSeconds;
+            long ElapsedTime = 0, MinTime = long.MaxValue, MaxTime = long.MinValue, IterationElapsedTime;
+
+            for (int i = 0; i < (NIter + 2); ++i)
+            {
+
+                long start = Stopwatch.GetTimestamp();
+                SortChoose(c,t); //METODA DO WYBIERANIA SORTOWANIA ZAMIAST JEDNEJ KONKRETNEJ
+                long end = Stopwatch.GetTimestamp();
+                IterationElapsedTime = end - start;
+                ElapsedTime += IterationElapsedTime;
+                if (IterationElapsedTime > MaxTime) MaxTime = IterationElapsedTime;
+                if (IterationElapsedTime < MinTime) MinTime = IterationElapsedTime;
+            }
+            ElapsedTime -= MaxTime + MinTime;
+            ElapsedSeconds = ElapsedTime * (1.0 / (NIter * Stopwatch.Frequency));
+            Console.Write("\t" + ElapsedSeconds.ToString("F10"));
+            return ElapsedSeconds.ToString("F10");
+        }
+        static void SortChoose(int c, int[] t)
+        {
+            switch (c)
+            {
+                case 0:
+                    //Console.WriteLine("HeapSort");
+                    sort.HeapSort(t);
+                    break;
+                case 1:
+                   // Console.WriteLine("CocktaSort");
+                    sort.CocktailSort(t);
+                    break;
+                case 2:
+                    //Console.WriteLine("InsertionSort");
+                    sort.InsertionSort(t);
+                    break;
+                case 3:
+                    //Console.WriteLine("SelectionSort");
+                    sort.SelectionSort(t);
+                    break;
+                case 4:
+                    sort.qsort(t, 0, t.Length - 1);
+                    break;
+                case 5:
+                    sort.qsort_it(t, 0);
+                    break;
+                default:
+                    break;
+            }
+        }
+        static string TableChoose(int c)
+        {
+            switch (c)
+            {
+                case 0:
+                    return "Desc";  
+                case 1:
+                    return "Ascd";
+                case 2:
+                    return "Rand";
+                case 3:
+                    return "V-sh";                 
+                case 4:
+                    return "Const";
+                default:
+                    return "";
+            }
+        }
+        static void GenChoose(int c, int[] t)
+        {
+            switch (c)
+            {
+                case 0:
+                    GenerateDescendingArray(t);
+                    break;
+                case 1:
+                    GenerateAscendingArray(t);
+                    break;
+                case 2:
+                    GenerateRandomArray(t, rnd);
+                    break;
+                case 3:
+                    GenerateVShape(t);
+                    break;
+                case 4:
+                    GenerateConst(t);
+                    break;
+                default:
+                    break;
+            }
+        }
+        static void BasicSorts()
+        {
+            Console.WriteLine("\t\tTIME ANALYSIS 4 DIFFRENT SORT ALGORITHMS");
+            //Console.WriteLine("Table\tHeapSort\t CoctailSort\t InsertionSort \t SelectionSort");
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\jakob\\OneDrive\\Pulpit\\c#\\Algorytmy\\Projekt3\\results.txt"))
+            {
+                for (int i = 10000; i < 85001; i += 5000)
+                {
+                    int[] x = new int[i];
+                    
+                    Console.Write("\n\nTable\tArray\tHeapSort\t CoctailSort\t InsertionSort \t SelectionSort");
+                    //Console.Write("\n" + x.Length);
+                    //writer.Write(x.Length + ";");
+                    for (int j = 0; j < 5; j++)
+                    {                       
+                        Console.Write("\n" + x.Length + "\t" + TableChoose(j));
+                        writer.Write("\n" + x.Length + ";" + TableChoose(j) + ";");
+                        for (int z = 0; z < 4; z++)
+                        {
+                            GenChoose(j, x); // PĘTLA FOR DLA ROZNYCH ROZMIAROW TABLIC TU MUSISZ ZROBIĆ
+                                             //foreach (int item in t) Console.Write(" " + item);
+                            writer.Write(SortTime(z, x) + ";");
+                        }
+                    }
+                }
+            }
+        }
+        static void QuickSortI()
+        {
+            Console.Write("\n\n");
+            Console.WriteLine("  TIME ANALYSIS 2 QUICK SORTS FOR RAND ARRAY");
+            Console.WriteLine("ARRAY\t RECURSIVE\tDIY STACK");
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\jakob\\OneDrive\\Pulpit\\c#\\Algorytmy\\Projekt3\\results_quick.txt"))
+            {
+                for (int i = 50000; i < 200001; i += 10000) //CZY DLA RÓŻNYCH TABLIC CZY JEDNEJ ROZMIAR TABLICY?   
+                {
+                    int[] x = new int[i];
+                    Console.Write(x.Length);
+                    writer.Write(x.Length + ";");
+                    for (int j = 4; j < 6; j++)
+                    {
+                        GenerateRandomArray(x, rnd);
+                        writer.Write(SortTime(j, x) + ";");
+                    }
+                    writer.Write('\n');
+                    Console.Write("\n");
+                }
+            }
+        }
+        static void QuickSortIIKey()
+        {
+            Console.Write("\n");
+            Console.WriteLine("QUICK SORT WITH DIFFRENT KEY ANALYSIS");
+            Console.WriteLine("Table\tMedian\t\tExtremeRight\tRandomPivot");
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\jakob\\OneDrive\\Pulpit\\c#\\Algorytmy\\Projekt3\\results_pivot_key.txt"))
+            {
+                for (int i = 50000; i < 100001; i += 5000) //CZY DLA RÓŻNYCH TABLIC CZY JEDNEJ ROZMIAR TABLICY?
+                {
+                    int[] x = new int[i];
+                    writer.Write(x.Length);
+                    Console.Write(x.Length);
+                    for (int j = 0; j < 3; j++)
+                    {
+                        GenerateDescendingArray(x);
+                        writer.Write(";" + SortTimeQuick(j, x));
+                    }
+                    Console.Write("\n");
+                    writer.Write('\n');
+                }
+            }
+        }
+        static void Tester() // QUICK SORT //
+        {
+            QuickSortI(); // BADANIE 1 częsci polecenia III
+            QuickSortIIKey(); // BADANIE 2 częsci polecenia III
+        }
+        static void Main(string[] args)
+        {
+            BasicSorts(); //BADANIE I, II polecenia TABLICE [50 - 200k] W PĘTLI
+            /////////////////////////////// QUICK SORT //////////////////////////////////////
+            Thread TesterThread = new Thread(Program.Tester, 8 * 1024 * 1024); // utworzenie wątku
+            TesterThread.Start(); // uruchomienie wątku
+            TesterThread.Join(); // oczekiwanie na zakończenie wątku
+
+        }
+    }
+}
---
+++ algo_data_structures/sorts.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt3
+{
+    internal class sorts
+    {
+        public void InsertionSort(int[] t)
+        {
+            for (uint i = 1; i < t.Length; i++)
+            {
+                uint j = i; // elementy 0 .. i-1 są już posortowane
+                int Buf = t[j]; // bierzemy i-ty (j-ty) element
+                while ((j > 0) && (t[j - 1] > Buf))
+                { // przesuwamy elementy
+                    t[j] = t[j - 1];
+                    j--;
+                }
+                t[j] = Buf; // i wpisujemy na docelowe miejsce
+            }
+        } /* InsertionSort() */
+
+        public void SelectionSort(int[] t)
+        {
+            uint k;
+            for (uint i = 0; i < (t.Length - 1); i++)
+            {
+                int Buf = t[i]; // bierzemy i-ty element
+                k = i; // i jego indeks
+                for (uint j = i + 1; j < t.Length; j++)
+                    if (t[j] < Buf) // szukamy najmniejszego z prawej
+                    {
+                        k = j;
+                        Buf = t[j];
+                    }
+                t[k] = t[i]; // zamieniamy i-ty z k-tym
+                t[i] = Buf;
+            }
+        } /* SelectionSort() */
+
+        public void Heapify(int[] t, uint left, uint right)
+        { // procedura budowania/naprawiania kopca
+            uint i = left,
+            j = 2 * i + 1;
+            int buf = t[i]; // ojciec
+            while (j <= right) // przesiewamy do dna stogu
+            {
+                if (j < right) // wybieramy większego syna
+                    if (t[j] < t[j + 1]) j++;
+                if (buf >= t[j]) break;
+                t[i] = t[j];
+                i = j;
+                j = 2 * i + 1; // przechodzimy do dzieci syna
+            }
+            t[i] = buf;
+        } /* Heapify() */
+        public void HeapSort(int[] t)
+        {
+            uint left = ((uint)t.Length / 2),
+            right = (uint)t.Length - 1;
+            while (left > 0) // budujemy kopiec idąc od połowy tablicy
+            {
+                left--;
+                Heapify(t, left, right);
+            }
+            while (right > 0) // rozbieramy kopiec
+            {
+                int buf = t[left];
+                t[left] = t[right];
+                t[right] = buf; // największy element
+                right--; // kopiec jest mniejszy
+                Heapify(t, left, right); // ale trzeba go naprawić
+            }
+        } /* HeapSort() */
+
+        public void CocktailSort(int[] t)
+        {
+            int Left = 1, Right = t.Length - 1, k = t.Length - 1;
+            do
+            {
+                for (int j = Right; j >= Left; j--) // przesiewanie od dołu
+                    if (t[j - 1] > t[j])
+                    {
+                        int Buf = t[j - 1]; t[j - 1] = t[j]; t[j] = Buf;
+                        k = j; // zamiana elementów i zapamiętanie indeksu
+                    }
+                Left = k + 1; // zacieśnienie lewej granicy
+                for (int j = Left; j <= Right; j++) // przesiewanie od góry
+                    if (t[j - 1] > t[j])
+                    {
+                        int Buf = t[j - 1]; t[j - 1] = t[j]; t[j] = Buf;
+                        k = j; // zamiana elementów i zapamiętanie indeksu
+                    }
+                Right = k - 1; // zacieśnienie prawej granicy
+            }
+            while (Left <= Right);
+        } /* CocktailSort() */
+        public void qsort(int[] t, int l, int p)
+        {
+            int i, j, x;
+            i = l;
+            j = p;
+            x = t[(l + p) / 2]; // (pseudo)mediana
+            do
+            {
+                while (t[i] < x) i++; // przesuwamy indeksy z lewej
+                while (x < t[j]) j--; // przesuwamy indeksy z prawej
+                if (i <= j) // jeśli nie minęliśmy się indeksami (koniec kroku)
+                { // zamieniamy elementy
+                    int buf = t[i]; t[i] = t[j]; t[j] = buf;
+                    i++; j--;
+                }
+            }
+            while (i <= j);
+            if (l < j) qsort(t, l, j); // sortujemy lewą część (jeśli jest)
+            if (i < p) qsort(t, i, p); // sortujemy prawą część (jeśli jest)
+        } /* qsort() */
+        public void qsort_it(int[] t, int swi)
+        {
+            int i, j, l, p, sp;
+            int[] stos_l = new int[t.Length],
+            stos_p = new int[t.Length]; // przechowywanie żądań podziału
+            sp = 0; stos_l[sp] = 0; stos_p[sp] = t.Length - 1; // rozpoczynamy od całej tablicy
+            do
+            {
+                l = stos_l[sp]; p = stos_p[sp]; sp--; // pobieramy żądanie podziału
+                do
+                {
+                    int x;
+                    i = l; j = p;
+                    //x = t[(l + p) / 2];// analogicznie do wersji rekurencyjnej
+                    //x = t[p];
+                    //x = t[pivot = rnd.Next() % (p - l) + l];
+                    x = t[DiffPivot(l, p, swi)]; // WYBIERANIE KLUCZA 
+                    do
+                    {
+                        while (t[i] < x) i++;
+                        while (x < t[j]) j--;
+                        if (i <= j)
+                        {
+                            int buf = t[i]; t[i] = t[j]; t[j] = buf;
+                            i++; j--;
+                        }
+                    } while (i <= j);
+                    if (i < p) { sp++; stos_l[sp] = i; stos_p[sp] = p; } // ewentualnie dodajemy żądanie podziału
+                    p = j;
+                } while (l < p);
+            } while (sp >= 0); // dopóki stos żądań nie będzie pusty
+        } /* qsort_it() */
+
+        public int RandPivot(int l, int p) // FUNKCJA DO LOSOWEGO KLUCZA
+        {
+            Random rnd = new Random();
+            int pivot = rnd.Next() % (p - l) + l;
+            return pivot;
+        }
+        public int DiffPivot(int l, int p, int i)  // FUNKCJA WYBIERAJĄCA KLUCZ
+        {
+            switch (i)
+            {
+                case 0:
+                    return (l + p) / 2;
+                case 1:
+                    return p;
+                case 2:
+                    return RandPivot(l, p);
+                default:
+                    break;
+            }
+            return 0;
+        }
+    }
+}
