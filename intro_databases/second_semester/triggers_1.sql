06afa00cd56915d93548d6c84f322190b47307e1
kinsin5 <jakobgrob9@gmail.com>
2025-03-22 21:29:33 +0100

    triggers_2 started

--- intro_databases/second_semester/triggers_1.sql
+++ intro_databases/second_semester/triggers_1.sql
@@ -2,9 +2,10 @@
 /*1. Utwórz relację AKTORZY_OPERACJE(data DATE, oper VARCHAR(100)), która posłuży
 do śledzenia operacji wykonywanych przez użytkownika. */
 CREATE TABLE AKTORZY_OPERACJE (
-	data DATE,
+	data TIMESTAMP,
 	oper VARCHAR(100)
 )
+DROP TABLE AKTORZY_OPERACJE;
 /*2  Utwórz wyzwalacz reagujący na operacje wprowadzania i usuwania krotek z relacji
 AKTORZY. Czas i rodzaj każdej operacji wykonywanej na relacji AKTORZY powinien
 być zapisywany w relacji AKTORZY_OPERACJE. Przetestuj działanie wyzwalacza.
@@ -14,7 +15,7 @@ CREATE OR REPLACE FUNCTION trig_procedure() RETURNS TRIGGER AS
 $$
 BEGIN
 	INSERT INTO aktorzy_operacje
-	VALUES ('now', TG_OP);
+	VALUES (now(), TG_OP);
 RETURN NEW;
 END;
 $$ LANGUAGE PLPGSQL;
@@ -29,7 +30,7 @@ INSERT INTO aktorzy(id_aktora, imie, nazwisko)
 VALUES (50, 'Jan', 'Kowalski');
 
 DELETE FROM aktorzy
-WHERE 
+WHERE id_aktora = 50;
 /* 3. Usuń wyzwalacz i procedurę wyzwalacza */
 
 DROP TRIGGER trigger_aktorzy
