f2fab3761ab6d4481d08f00d3050d2b9fd2d36ea
Jakub Grobelny <72096815+kinsin5@users.noreply.github.com>
2025-03-23 08:43:11 +0100

    Update triggers_2.sql

--- intro_databases/second_semester/triggers_2.sql
+++ intro_databases/second_semester/triggers_2.sql
@@ -30,7 +30,63 @@ $$ LANGUAGE PLPGSQL;
 
 SELECT nazwisko, pensja, pit(pensja) FROM pracownicy;
 
+/*2. Utwórz w języku PL/pgSQL funkcję HIRE(id_pracownika INT, imie VARCHAR, nazwisko
+VARCHAR, miasto VARCHAR, pensja REAL) służącą do wstawienia nowego rekordu do tabeli
+PRACOWNICY. Funkcja powinna zwracać wartość tekstową:
+- "OK", jeżeli wstawianie rekordu odbędzie się bez zgłoszenia wyjątku
+- "DUPLIKAT ID", jeżeli podczas próby wstawienia rekordu zostanie zgłoszony wyjątek
+UNIQUE_VIOLATION
+Następnie sprawdź działanie funkcji HIRE():
+select hire(6, 'Jan', 'Kowalski', 'Poznan', 100);
+select hire(5, 'Anna', 'Nowak', 'Poznan', 200); 
+ */
+ 
+CREATE OR REPLACE FUNCTION HIRE
+ 	(id_pracownika INT, imie VARCHAR, nazwisko VARCHAR, miasto VARCHAR, pensja REAL)
+RETURNS VARCHAR AS
+$$
+BEGIN
+	EXECUTE 
+		FORMAT('INSERT INTO pracownicy(id_pracownika, imie, nazwisko, miasto, pensja) 
+			   VALUES ($1, $2, $3, $4, $5)') USING id_pracownika, imie, nazwisko, miasto, pensja;
+	RETURN 'OK';
+EXCEPTION
+	WHEN UNIQUE_VIOLATION THEN
+		RETURN 'DUPLIKAT ID';
+END;
+$$ LANGUAGE PLPGSQL;
+
+SELECT HIRE(7, 'Anna', 'Nowak', 'Poznan', 200);
+select hire(5, 'Anna', 'Nowak', 'Poznan', 200); 
+
+SELECT * FROM pracownicy;
+
+DELETE FROM pracownicy
+WHERE id_pracownika = 7;
 
 
+/*
+3. Utwórz wyzwalacz DOUBLE_SALARY i związaną z nim procedurę wyzwalaną
+DOUBLE_SALARY_FUN, który każdemu nowo wstawianemu do tabeli PRACOWNICY rekordowi
+podwoi wartość kolumny PENSJA.
+Następnie sprawdź działanie wyzwalacza:
+insert into pracownicy values(10, 'Jan', 'Kowalski', 'Poznan', 100);
+select * from pracownicy where id_pracownika=10;
+*/
 
-/* */
+CREATE OR REPLACE FUNCTION DOUBLE_SALARY_FUN()
+RETURNS TRIGGER AS 
+$$
+BEGIN
+	NEW.pensja := NEW.pensja * 2;
+	RETURN NEW;
+END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER DOUBLE_SALARY
+BEFORE INSERT ON pracownicy
+FOR EACH ROW EXECUTE PROCEDURE DOUBLE_SALARY_FUN();
+
+insert into pracownicy values(10, 'Jan', 'Kowalski', 'Poznan', 100);
+select * from pracownicy where id_pracownika=10;
+
